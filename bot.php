9d5c00db9857e52b6a3e1685f029783621b7467f
partitan <partitan@gmail.com>
2016-11-25 22:44:29 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -35,13 +35,15 @@ if (!is_null($events['events'])) {
 			
 			//$tweetreply = new TwitterOAuth2($consumerKey, $consumerSecret, $oAuthToken, $oAuthSecret);
 			//$tweetreply ->get('direct_messages', array('count' => 1));
+			
 			$msgReply = 'test test test test';
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
+				//'messages' => [$messages],
+				'messages' => [$msgReply],
 			];		
 			
 			$post = json_encode($data);
