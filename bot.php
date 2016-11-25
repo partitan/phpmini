e07a9f1abe76ea29d63e818d2036a1235cfae943
partitan <partitan@gmail.com>
2016-11-25 14:55:24 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,8 +19,14 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => $text
 			];
+					
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
 			
-		
 			//Twitter APP
 			$consumerKey    = '4MTtBPsekfyeS1gGLIndqbWbC';
 			$consumerSecret = 'WGBrMsZacLjCp3KPGhxyvDHske0N9YS1ZZiewnaGOXVpfst04J';
@@ -28,15 +34,11 @@ if (!is_null($events['events'])) {
 			$oAuthSecret    = 'NUqaIFUzx5iiPkMsqlEvzhJe3hrO1JXxLQ2mwvYX36gph';
 			require_once('twitteroauth.php');
 			$tweet = new TwitterOAuth($consumerKey, $consumerSecret, $oAuthToken, $oAuthSecret);
-			$tweet->post('direct_messages/new', array('screen_name' => 'prungkrae', 'text' => $messages ));
+			$tweet->post('direct_messages/new', array('screen_name' => 'prungkrae', 'text' => $data ));
 					
 			
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
+			
+			
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 			$ch = curl_init($url);
