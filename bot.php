adb7ad02c329d7f917d96039329581d064c7bdd4
partitan <partitan@gmail.com>
2016-11-27 11:46:28 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -31,7 +31,7 @@ if (!is_null($events['events'])) {
 			$tweet = new TwitterOAuth($consumerKey, $consumerSecret, $oAuthToken, $oAuthSecret);
 			$tweet->post('direct_messages/new', array('screen_name' => 'krungthepCXP', 'text' =>$messages));
 					
-			sleep(30);
+			sleep(5);
 			
 			//$tweetreply = new TwitterOAuth2($consumerKey, $consumerSecret, $oAuthToken, $oAuthSecret);
 			//$tweetreply ->get('direct_messages', array('count' => 1));
@@ -63,7 +63,7 @@ if (!is_null($events['events'])) {
     			}
 			
 			
-			sleep(5);
+			//sleep(5);
 			//////// Sunday End......
 			
 			
