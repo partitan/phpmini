f47ab9d5b016adb8ce26d7d4bd1c8ed179fd3cf4
partitan <partitan@gmail.com>
2018-09-21 08:38:20 +0700

    Update 4line.php

--- 4line.php
+++ 4line.php
@@ -1,12 +1,8 @@
 <?php
     $accessToken = "CEdTKiv9KD/q/C4xWu9hEUbI/RxXH026qprWmcyqgGwuFz6fXfXPuo+ND2WPBNiGDgjSRjtXHiZDaKp62GfpwKjm5/GBdcI4LuvzkB1rFfTzSBtkLaN6/xxE9EU8A0/Mf4+rQb4ZiMOH9eij2kFeFQdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
 	
-	//
-//$access_token = 'CEdTKiv9KD/q/C4xWu9hEUbI/RxXH026qprWmcyqgGwuFz6fXfXPuo+ND2WPBNiGDgjSRjtXHiZDaKp62GfpwKjm5/GBdcI4LuvzkB1rFfTzSBtkLaN6/xxE9EU8A0/Mf4+rQb4ZiMOH9eij2kFeFQdB04t89/1O/w1cDnyilFU=';
-
 //$channelSecret = 'e75e3b8c53e2943ed4f419fe5a7688bc';
 
-    
     $content = file_get_contents('php://input');
     $arrayJson = json_decode($content, true);
     
