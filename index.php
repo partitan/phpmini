85999a137e2bdf24a217bed94f38e3f92c9cd6dc
partitan <partitan@gmail.com>
2019-09-11 14:16:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
 <?php
-echo "Hello LINE BOT";
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+?> 
+
+</body>
+</html>
