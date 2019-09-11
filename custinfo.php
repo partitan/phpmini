1d98bbcd3d01c9a8342b089ac9888ce5bdb7eef4
partitan <partitan@gmail.com>
2019-09-11 14:40:37 +0700

    Update and rename phone.php to custinfo.php

---
+++ custinfo.php
@@ -0,0 +1,11 @@
+<HTML> 
+ <HEAD><TITLE> Result </TITLE></HEAD> 
+ <BODY> 
+        <?php
+        $ani =  $_POST['ani];
+        $custid = $_POST['custid'];
+        ?>
+                <P> Customer Phone = <?php echo "$ani"; ?> 
+                <BR> Customer ID = <?php echo "$custid"; ?> 
+ </BODY> 
+</HTML>
