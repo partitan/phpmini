bffd83dbb330d4384ac6b4e298dfd803c655fc67
partitan <partitan@gmail.com>
2019-10-07 15:10:52 +0700

    Update custinfo.php

--- custinfo.php
+++ custinfo.php
@@ -1,7 +1,11 @@
 <HTML> 
  <HEAD><TITLE> Result </TITLE></HEAD> 
  <BODY> 
+  
                 <P> Customer Phone = <?php echo $_GET['ani']; ?>
                 <BR> Customer ID = <?php echo $_GET['custid']; ?>
+  
+                 <iframe src="https://www.w3schools.com"></iframe>
+  
  </BODY> 
 </HTML>
