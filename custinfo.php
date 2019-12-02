89f094d75060325769f499eacf8588ae6b75870e
partitan <partitan@gmail.com>
2019-12-02 12:52:07 +0700

    Update custinfo.php

--- custinfo.php
+++ custinfo.php
@@ -5,10 +5,6 @@
   <H1>Simulate Sugar CRM Application</H1><BR>
                 <P> Customer Phone = <?php echo $_GET['ani']; ?>
                 <BR> Incoming_channel = <?php echo $_GET['dnis']; ?>
-                //<BR> Incoming_Hospital = <?php echo $_GET['hospital_code']; ?>
-                //<BR> Call_in_flag = <?php echo $_GET['flag']; ?>
-                //<BR> Call_typeg = <?php echo $_GET['call_type']; ?>
-                 
                 <BR> <iframe width="800" height="800" src="./form.html" frameborder="0" allowfullscreen></iframe>
   
  </BODY> 
