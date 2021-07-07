54a812e498ee53c37f7f7c5783cd00eea2ab0010
partitan <partitan@gmail.com>
2021-07-07 12:32:40 +0700

    Update main.js

--- html5up-paradigm-shift/assets/js/main.js
+++ html5up-paradigm-shift/assets/js/main.js
@@ -4,6 +4,18 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+
+  (function (g, e, n, es, ys) {
+    g['_genesysJs'] = e;
+    g[e] = g[e] || function () {g[e].q = g[e].q || []).push(arguments)};
+    g[e].t = 1 * new Date();
+    g[e].c = es;
+    ys = document.createElement('script'); ys.async = 1;
+	  ys.src = n; ys.charset = 'utf-8'; 
+	  document.head.appendChild(ys);
+  })(window, 'Genesys', 'https://apps.mypurecloud.com.au/genesys-bootstrap/genesys.min.js', {environment: 'apse2',deploymentId: 'dbba73ea-c955-4604-948c-a603930ae1a1'});
+
+
 (function($) {
 
 	var	$window = $(window),
